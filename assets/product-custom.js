e9a8f654ebd886b4af29b5f1cf60c58362554ac0
VERVHQ <m@vervhq.com>
2026-01-21 16:05:30 +0500

    Fix JS: Rewrite with zoom preview

--- assets/product-custom.js
+++ assets/product-custom.js
@@ -1,7 +1,43 @@
 class ProductCustomGallery extends HTMLElement {
-        // Update Active State
+    constructor() {
+        super();
+        this.thumbnails = this.querySelectorAll('.product-custom__thumbnail');
+        this.mainImageWrapper = this.querySelector('.product-custom__main-image-wrapper');
+        this.mainImage = this.querySelector('.product-custom__main-image');
+    }
+
+    connectedCallback() {
+        this.thumbnails.forEach(thumbnail => {
+            thumbnail.addEventListener('click', () => this.updateMainImage(thumbnail));
+        });
+
+        // Zoom preview on hover
+        if (this.mainImageWrapper && this.mainImage) {
+            this.mainImageWrapper.addEventListener('mousemove', (e) => this.handleZoomMove(e));
+            this.mainImageWrapper.addEventListener('mouseleave', () => this.handleZoomLeave());
+        }
+    }
+
+    updateMainImage(thumbnail) {
+        const newSrc = thumbnail.dataset.targetSrc;
+        const newAlt = thumbnail.dataset.targetAlt;
+        if (this.mainImage) {
+            this.mainImage.src = newSrc;
+            this.mainImage.alt = newAlt;
+        }
         this.thumbnails.forEach(t => t.classList.remove('active'));
-target.classList.add('active');
+        thumbnail.classList.add('active');
+    }
+
+    handleZoomMove(e) {
+        const rect = this.mainImageWrapper.getBoundingClientRect();
+        const x = ((e.clientX - rect.left) / rect.width) * 100;
+        const y = ((e.clientY - rect.top) / rect.height) * 100;
+        this.mainImage.style.transformOrigin = `${x}% ${y}%`;
+    }
+
+    handleZoomLeave() {
+        this.mainImage.style.transformOrigin = 'center center';
     }
 }
 
@@ -12,23 +48,22 @@ class ProductTabs extends HTMLElement {
         super();
         this.buttons = this.querySelectorAll('.product-tabs__btn');
         this.panes = this.querySelectorAll('.product-tabs__pane');
+    }
 
-        this.buttons.forEach(btn => {
-            btn.addEventListener('click', this.handleTabClick.bind(this));
+    connectedCallback() {
+        this.buttons.forEach(button => {
+            button.addEventListener('click', () => this.switchTab(button));
         });
     }
 
-    handleTabClick(event) {
-        const target = event.currentTarget;
-        const contentId = target.dataset.targetId;
+    switchTab(button) {
+        const targetId = button.dataset.targetId;
 
-        // Reset all
         this.buttons.forEach(btn => btn.classList.remove('active'));
         this.panes.forEach(pane => pane.classList.remove('active'));
 
-        // Activate target
-        target.classList.add('active');
-        const targetPane = this.querySelector(`#${contentId}`);
+        button.classList.add('active');
+        const targetPane = this.querySelector(`#${targetId}`);
         if (targetPane) {
             targetPane.classList.add('active');
         }
